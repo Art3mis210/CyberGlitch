8b4d85f1bfc6831aed7a61eb701bdfe2fc30ec8b
Art3mis210 <77666553+Art3mis210@users.noreply.github.com>
2022-08-28 12:42:13 +0530

    Tutorial lighting

--- Assets/Scripts/Player/GrapplingHook.cs
+++ Assets/Scripts/Player/GrapplingHook.cs
@@ -81,12 +81,9 @@ public class GrapplingHook : MonoBehaviour
         lineR.positionCount = 0;
         GrappleMode = false;
         Player.playerInstance.GrappleMode = false;
-<<<<<<< Updated upstream
         Destroy(joint);
-=======
         if (joint != null)
-        joint.spring = 0;
->>>>>>> Stashed changes
+            joint.spring = 0;
     }
     private void OnDisable()
     {
