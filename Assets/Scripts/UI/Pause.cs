fd1674c5d4b30bf7d6df8f66da8174117eab3a78
Art3mis210 <77666553+Art3mis210@users.noreply.github.com>
2022-11-20 00:26:24 +0530

    documented code

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -10,18 +10,18 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Escape))        //Pauses the game when escape is pressed
         {
             PauseMenu.SetActive(true);
             Time.timeScale = 0;
         }
     }
-    public void Resume()
+    public void Resume()                            //called from UI button to resume the game
     {
         PauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
-    public void Quit()
+    public void Quit()                              //called from UI button to quit the game
     { 
         Time.timeScale = 1;
         SceneManager.LoadScene("Lab");
